a6532e864f537586bf907cff9763c0096b8e80b6
YoSi9N <yosimune33333@yahoo.co.jp>
2019-12-06 12:44:34 +0900

    update message-ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,35 +3,34 @@ $(function(){
     // 「もしメッセージに画像が含まれていたら」という条件式
     if (message.image) {
       var html = `<div class="main__content__messages">
-      <div class="main__content__messages--poster">
-      <div class="main__content__messages--poster--name">
-      ${message.name}
-      </div>
-      <div class="main__content__messages--poster--time">
-      ${message.created_at}
-      </div>
-      </div>
-      <div class="main__content__messages--text">
-      ${message.text}
-      </div>
-      <img class="aaaa" src="${message.image}">
-      </div>`
+                    <div class="main__content__messages--poster">
+                      <div class="main__content__messages--poster--name">
+                       ${message.name}
+                      </div>
+                      <div class="main__content__messages--poster--time">
+                       ${message.created_at}
+                      </div>
+                    </div>
+                     <div class="main__content__messages--text">
+                      ${message.text}
+                     </div>
+                     <img class="aaaa" src="${message.image}">
+                  </div>`
 
     } else {
       var html = `<div class="main__content__messages">
-        <div class="main__content__messages--poster">
-          <div class="main__content__messages--poster--name">
-           ${message.name}
-          </div>
-          <div class="main__content__messages--poster--time">
-            ${message.created_at}
-          </div>
-        </div>
-        <div class="main__content__messages--text">
-        ${message.text}
-        </div>
-      
-      </div>`
+                    <div class="main__content__messages--poster">
+                      <div class="main__content__messages--poster--name">
+                        ${message.name}
+                      </div>
+                      <div class="main__content__messages--poster--time">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="main__content__messages--text">
+                      ${message.text}
+                    </div>
+                  </div>`
     }
     return html
   }
@@ -52,7 +51,7 @@ $(function(){
       var html = buildHTML(data);
       $(".main__content").append(html);
       $('.main__content').animate({ scrollTop: $('.main__content')[0].scrollHeight});
-      $(".main__footer__form__input-box--text").val('');
+      $(".main__footer__form")[0].reset();
       $(".main__footer__form--send").prop("disabled", false);
     })
     .fail(function(){
